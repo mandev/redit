de049ab8236b475b14a85c0756147f529f06d10c
deviller <burma@free.fr>
2019-10-09 00:18:21 +0200

    Add file header

--- src/main/java/com/adlitteram/redit/gui/UploadListener.java
+++ src/main/java/com/adlitteram/redit/gui/UploadListener.java
@@ -1,14 +1,26 @@
-/*
- * UploadDialog.java
- *
- * Created on 26 novembre 2005, 17:06
- *
- * To change this template, choose Tools | Options and locate the template under
- * the Source Creation and Management node. Right-click the template and choose
- * Open. You can then make changes to the template in the Source Editor.
- */
 package com.adlitteram.redit.gui;
 
+/*-
+ * #%L
+ * rEdit
+ * %%
+ * Copyright (C) 2009 - 2019 mandev
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
 import com.adlitteram.jasmin.Message;
 import com.adlitteram.jasmin.io.ProgressListener;
 import java.awt.AWTEvent;
--- src/main/java/com/adlitteram/redit/gui/listener/MainframeListener.java
+++ src/main/java/com/adlitteram/redit/gui/listener/MainframeListener.java
@@ -1,13 +1,26 @@
-/*
- * MainframeListener.java
- *
- * Created on 1 mars 2007, 11:12
- *
- * To change this template, choose Tools | Template Manager
- * and open the template in the editor.
- */
 package com.adlitteram.redit.gui.listener;
 
+/*-
+ * #%L
+ * rEdit
+ * %%
+ * Copyright (C) 2009 - 2019 mandev
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
 import com.adlitteram.redit.AppManager;
 import com.adlitteram.redit.action.Quit;
 import com.adlitteram.redit.gui.MainFrame;
